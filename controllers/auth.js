1014af3cfbbb1fd949b4568b25517485d42ea40c
Mazen Ramadan <mazenramadan00@gmail.com>
2022-08-06 23:59:56 +0200

    JWT tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
     const {name, email, password} = req.body
@@ -10,7 +11,10 @@ const register = async (req, res) => {
 
     const tempUser = {name, email, password: hashedPassword}
     const user = await User.create({...tempUser} )
-    res.status(StatusCodes.CREATED).json({ user });
+    const token = jwt.sign({userId:user._id, name:user.name}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME,
+    })
+    res.status(StatusCodes.CREATED).json({user:{name: user.name}, token });
 };
 
 const login = async (req, res) => {
