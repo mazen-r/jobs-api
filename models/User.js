88c74d0abd51a0324677ff3d6fd0d5ccf8d0e251
Mazen Ramadan <mazenramadan00@gmail.com>
2022-08-06 15:13:40 +0200

    user Model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:[true, 'Please provide a name'],
+        minlength: 3,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        match: [ // used to validate the email
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        'please provide a valid email'
+        ],
+        unique: [true, 'Email is already used']
+    },
+    password: {
+        type: String,
+        required: [true, 'please provide a password'],
+        minlength: 6
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema);
