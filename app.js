e31e27d88712b02acbeace47832e27fec75ee32b
Mazen Ramadan <mazenramadan00@gmail.com>
2022-08-06 13:20:01 +0200

    connect to MognoDB

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const connectDB = require('./db/connect')
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Jobs API");
-});
+const authRoutes = require('./routes/auth');
+const jobsRoutes = require('./routes/jobs')
+
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/jobs', jobsRoutes);
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -20,6 +24,7 @@ const port = process.env.port || 8000
 
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, () => 
         console.log(`Server is running on port ${port}`));
     } catch (error) {
