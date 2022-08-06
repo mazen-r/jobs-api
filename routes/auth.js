bd0d9b206661a250d69ba3c4c3b6c7638528434b
Mazen Ramadan <mazenramadan00@gmail.com>
2022-08-06 15:14:00 +0200

    register user

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,7 @@ const {login, register} = require('../controllers/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-
+router.get('/hello', (req, res) => {
+    res.send("Hello")
+})
 module.exports = router;
