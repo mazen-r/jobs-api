f3fa82a33fb276292118e4f70d57d383d2d39fe7
Mazen Ramadan <mazenramadan00@gmail.com>
2022-08-08 17:19:30 +0200

    delete job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -50,10 +50,21 @@ const updateJob = async (req, res) => {
     res.status(StatusCodes.OK).json({ job })
   }
 
-const deleteJob = async (req, res) => {
-    res.send('delete job')
-}
-
+  const deleteJob = async (req, res) => {
+    const {
+      user: { userId },
+      params: { id: jobId },
+    } = req
+  
+    const job = await Job.findByIdAndRemove({
+      _id: jobId,
+      createdBy: userId,
+    })
+    if (!job) {
+      throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).send()
+  }
 module.exports = {
     getAllJobs,
     getJob,
