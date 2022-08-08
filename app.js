5a5e935033587c15407d2c087427782f65753721
Mazen Ramadan <mazenramadan00@gmail.com>
2022-08-08 14:34:05 +0200

    authentication middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
 
 app.use(express.json());
 
@@ -12,7 +13,7 @@ const authRoutes = require('./routes/auth');
 const jobsRoutes = require('./routes/jobs')
 
 app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/jobs', jobsRoutes);
+app.use('/api/v1/jobs', authenticateUser, jobsRoutes);
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
