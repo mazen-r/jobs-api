4ef9c404ce4da90615b4b4550e63b2ce1ae9b1a5
Mazen Ramadan <mazenramadan00@gmail.com>
2022-08-16 13:53:29 +0200

    More login tests

---
+++ tests/auth.test.js
@@ -0,0 +1,33 @@
+const request = require('supertest')
+const app = require('../server')
+
+describe("test user login", () => {
+
+    test("it should responses with status 200", async () => {
+        const res = await request(app).post('/api/v1/auth/login')
+        .send({
+            email: "test@email.com",
+            password: "secret"
+        })
+        expect(res.status).toEqual(200)
+    })
+    
+    test("it should return invalid credentials with status 401", async () => {
+        const res = await request(app).post('/api/v1/auth/login')
+        .send({
+            email:"test@email.com",
+            password: "wrongpass"
+        })
+        expect(res.status).toEqual(401)
+        expect(res.text).toEqual('{"msg":"Invalid Credentials"}')
+    })
+
+    test("it should return Please provide email and password with status 400", async () => {
+        const res = await request(app).post('/api/v1/auth/login')
+        .send({
+            email:"test@email.com"
+        })
+        expect(res.status).toEqual(400)
+        expect(res.text).toEqual('{"msg":"Please provide email and password"}')
+    })
+})
